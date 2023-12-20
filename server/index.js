eb65bd91fb49ef0a0c00c09eeefc76b945e3e21d
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-20 20:19:36 +0530

    MongoDB Setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config();
 
 const app = express()
+const mongodbUrl = process.env.MONGODB_URL
+mongoose.connect(mongodbUrl)
 
 app.get('/test',(req,res)=>{
     res.json('Test OK');
 })
 
 app.post('/register',(req,res)=>{
-    
+
 })
 
 app.listen(4000)
