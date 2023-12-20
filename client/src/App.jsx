b63d91d13ed802e9ca1dfb578cd756667be1b3c1
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-20 17:30:18 +0530

    Starting Registration Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import Register from "./Register"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-red-500'>Hello</div>
+      <Register />
     </>
   )
 }
---
+++ client/src/Register.jsx
@@ -0,0 +1,14 @@
+const Register = () => {
+    return (
+        <>
+            <div className="bg-blue-50 h-screen">
+                <form>
+                    <input type="text" placeholder="username" />
+                    <input type="password" placeholder="password" />
+                </form>
+            </div>
+        </>
+    )
+};
+
+export default Register;
