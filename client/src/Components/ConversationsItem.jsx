f6b74e1470e5ccdee4b3ff6a1852c4fc537a1f82
Kumar Shresth <kumarshresth2004@gmail.com>
2023-12-30 16:16:10 +0530

    #

--- client/src/Components/ConversationsItem.jsx
+++ client/src/Components/ConversationsItem.jsx
@@ -3,15 +3,20 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Icon, IconButton } from '@mui/material';
 
 
+
 function ConversationsItem({props}) {
 
   
   return (
     <div className='conversation-container'>
-      <p className="con-icon">{props.name[0]}</p>
-      <p className="con-title">{props.name}</p>
-      <p className="con-lastMessage">{props.lastMessage}</p>
-      <p className="con-timeStamp">{props.timeStamp}</p>
+      <p className="con-icon">
+        <IconButton className='acc-icon'>
+          <AccountCircleIcon/>
+          {props.name}
+        </IconButton>
+    </p>
+
+      {/* <p className="con-title">{props.name}</p> */}
     </div>
   )
 }
--- client/src/Components/WorkArea.jsx
+++ client/src/Components/WorkArea.jsx
@@ -25,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function appendMessage(message){
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
+    messageContainer.append(messageElement);
   }
 });
 
