3dcf2bf68eb269b1da618bb8fbb57a1d0e894858
Kumar Shresth <kumarshresth2004@gmail.com>
2023-12-30 16:37:30 +0530

    #

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -14,20 +14,14 @@ function Sidebar() {
   const [conversations, setconversations] = useState([
     {
       name:"Test#1",
-      lastMessage:"Last Message #1",
-      timeStamp:"today",
     },
 
     {
       name:"Test#2",
-      lastMessage:"Last Message #2",
-      timeStamp:"today",
     },
 
     {
       name:"Test#3",
-      lastMessage:"Last Message #3",
-      timeStamp:"today",
     },
   ])
   
