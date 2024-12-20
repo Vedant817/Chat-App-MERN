4460ffe60f6e2e1d032cb4451ac7d13c76ad3c16
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-12-20 15:01:20 +0530

    Sending Emails using Resend

--- server/controllers/subscriptionController.js
+++ server/controllers/subscriptionController.js
@@ -1,4 +1,22 @@
+import { Resend } from 'resend';
 import Subscription from '../models/Subscription.js';
+
+const Resend_API_Key = process.env.RESEND_API_KEY || 're_ThedFaaG_5kzYQaJ8xoMUpxxc5Hi5ZjnW';
+const resend = new Resend(Resend_API_Key);
+
+const sendEmail = async (email) => {
+    const { data, error } = await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: email,
+        subject: "Thank you for subscribing",
+        html: "<h1>Thank you for subscribing HelloHub</h1><p>You will receive updates on our latest products and services</p>"
+    })
+
+    if (error) {
+        return res.status(400).json({ error });
+    }
+    return data;
+}
 
 const subscribe = async (req, res) => {
     try {
@@ -11,8 +29,15 @@ const subscribe = async (req, res) => {
         if (existingSubscription) {
             return res.status(201).json({ message: 'Email already subscribed' });
         }
-
         const createSubscription = await Subscription.create({ email });
+        if (!createSubscription) {
+            return res.status(500).json({ message: 'Error subscribing' });
+        }
+
+        const emailResult = await sendEmail(email);
+        if (!emailResult) {
+            return res.status(500).json({ message: 'Error sending welcome email' });
+        }
 
         res.status(201).json({ message: 'Subscribed successfully', subscription: existingSubscription });
     } catch (error) {
