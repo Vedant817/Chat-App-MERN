326159e1a6134440fa6aa3ac2bae611e26bc3193
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-12-23 18:38:59 +0530

    Logout Functionality

--- client/src/components/dashboard/Navbar.tsx
+++ client/src/components/dashboard/Navbar.tsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import AddFriendDialog from './AddFriendDialog'
+import { useNavigate } from 'react-router-dom'
 
 interface NavbarProps {
   username: string;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ username }) => {
-  const [isAddFriendOpen, setIsAddFriendOpen] = useState(false)
+  const [isAddFriendOpen, setIsAddFriendOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  }
 
   return (
     <nav className="flex items-center justify-between bg-background p-4 shadow w-screen">
@@ -17,9 +24,10 @@ const Navbar: React.FC<NavbarProps> = ({ username }) => {
         </a>
         <span className="text-sm text-muted-foreground">Welcome, {username}</span>
       </div>
-      <div>
+      <div className='flex space-x-1'>
         <Button onClick={() => setIsAddFriendOpen(true)}>Add Friend</Button>
         <AddFriendDialog open={isAddFriendOpen} onOpenChange={setIsAddFriendOpen} />
+        <Button className='bg-red-500' onClick={() =>logout()}>Logout</Button>
       </div>
     </nav>
   )
